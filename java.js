1d056c4a3818bf1c72a625a598545c796b807b47
HmzLhb <hamzalachehabe@gmail.com>
2022-12-27 16:23:43 +0100

    search in json works finally

--- java.js
+++ java.js
@@ -1,40 +1,30 @@
 // Create a new XMLHttpRequest object
 const request = new XMLHttpRequest();
 
-// Set the onreadystatechange event handler
-request.onreadystatechange = function() {
-  // Check the readyState and status of the request
-  if (request.readyState === 4 && request.status === 200) {
-    // Parse the response
-    const data = JSON.parse(request.responseText);
+// Attach an event listener to the element with the ID "SearchBar"
+document.getElementById("SearchBar").addEventListener("keyup", function () {
+  // Get the value of the element
+  const searchTerm = this.value;
 
-    // Get the table element
-    const table = document.getElementById('data-table');
+  // Set the onreadystatechange event handler
+  request.onreadystatechange = function() {
+    // Check the readyState and status of the request
+    if (request.readyState === 4 && request.status === 200) {
+      // Parse the response
+      const data = JSON.parse(request.responseText);
+    
 
-    // Iterate through the data
-    data.forEach(item => {
-      // Create a new table row
-      const row = document.createElement('tr');
+      // Filter the data by the search term
+      const filteredData = data.movies.filter(movie => movie.title.toLowerCase().includes(searchTerm));
 
-      // Create a cell for each property
-      const nameCell = document.createElement('td');
-      nameCell.innerText = item.name;
-      row.appendChild(nameCell);
-
-      const ageCell = document.createElement('td');
-      ageCell.innerText = item.age;
-      row.appendChild(ageCell);
-
-      // Repeat for other properties...
-
-      // Append the row to the table body
-      table.appendChild(row);
-    });
+      // Log the filtered data to the console
+      console.log(filteredData);
+    }
   }
-}
 
-// Open the request and set the HTTP method and URL
-request.open('GET', 'movies.json');
+  // Open the request and set the HTTP method and URL
+  request.open('GET', 'movies.json');
 
-// Send the request
-request.send();
+  // Send the request
+  request.send();
+});
