ca3913e3d9c53c3dc32927773ed499bfd7b57df2
HmzLhb <hamzalachehabe@gmail.com>
2022-12-29 04:42:38 +0100

    now the sorting works let get some looks

--- java.js
+++ java.js
@@ -107,3 +107,55 @@ document.getElementById("SearchBar").addEventListener("keyup", function() {
       request.send();
 
 });
+
+// sort table
+
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("table");
+  switching = true;
+  //Set the sorting direction to ascending:
+  dir = "asc"; 
+  //while loop that will continue until no switching has been done
+  
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    //loop through all table rows  without thead
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      //getting the value from the current cell and the next cell respecting the loop
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+     //check if the two rows should switch
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+    //log the switch value if true then switch and mark the as done
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++;      
+    } else {
+      //if the switch value is false, check if the direction is "asc" and set the direction to "desc" and run the while loop again.
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
